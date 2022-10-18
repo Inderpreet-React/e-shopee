831cd5964910a60b08f01f2aa2a61cd00a35bfc4
Inderpreet <inderpreet.react.project@gmail.com>
2022-10-18 12:55:59 +0530

    card summary added

--- src/components/ShoopingCart.js
+++ src/components/ShoopingCart.js
@@ -1,5 +1,30 @@
 import React from "react";
+import PageWrapper from "../PageWrapper";
+import shoppingCartSvg from "../images/shoppingCartSvg.svg";
+import SummaryCard from "./SummaryCard";
 
 export default function ShoopingCart() {
-	return <div>ShoopingCart</div>;
+	const shoopingCart = true;
+
+	return (
+		<PageWrapper>
+			{shoopingCart ? (
+				<div className="p-4 md:p-8  w-full md:w-1/2 h-full">
+					<div className="p-4 bg-white rounded border-2 border-gray-400 h-full flex-col flex gap-4 overflow-y-scroll">
+						<SummaryCard />
+					</div>
+				</div>
+			) : (
+				<div className="flex items-center justify-center h-full w-full flex-col">
+					<img src={shoppingCartSvg} alt="shopping" className="h-64 w-64" />
+					<h1 className="text-2xl font-semibold text-gray-600">
+						Looks like your shooping cart is empty...
+					</h1>
+					<p className="mt-4 text-gray-800">
+						Please have something soon, carts have feeling too.
+					</p>
+				</div>
+			)}
+		</PageWrapper>
+	);
 }
---
+++ src/components/SummaryCard.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function SummaryCard() {
+	const imgLink =
+		"https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1659004146_9433808.jpg?format=webp&w=300&dpr=1.3";
+	return (
+		<div className="flex gap-4">
+			<img
+				className="w-28 md:w-36 rounded-md h-auto"
+				src={imgLink}
+				alt="product"
+			/>
+			<div className="flex text-gray-600 justify-between w-full">
+				<div className="flex flex-col ">
+					<h3 className="text-lg font-semibold">Default Name</h3>
+					<h4 className="text-xs">Mens Jogger</h4>
+					<div className="flex gap-4 mt-4">
+						<span className="text-sm font-semibold border-2 border-gray-300 rounded p-1 ">
+							Size: xl
+						</span>
+						<span className="text-sm font-semibold border-2 border-gray-300 rounded p-1 ">
+							Qty: 1
+						</span>
+					</div>
+					<button className=" bg-indigo-500 rounded p-1 text-white min-w-min mt-4">
+						Remove
+					</button>
+				</div>
+			</div>
+			<div className="font-semibold">₹ 500/-</div>
+		</div>
+	);
+}
