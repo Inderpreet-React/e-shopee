502be0df9c1223ef5b1b92f6d2f24dc23938b5bb
Inderpreet <inderpreet.react.project@gmail.com>
2022-11-13 19:50:49 +0530

    added protected routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
@@ -7,7 +7,7 @@ import Home from "./pages/Home";
 import ShoopingCart from "./pages/ShoopingCart";
 import ProductDetails from "./pages/ProductDetails";
 import Wishlist from "./pages/Wishlist";
-import { onSnapshot, doc, updateDoc } from "firebase/firestore";
+import { onSnapshot, doc } from "firebase/firestore";
 import { db, auth } from "./firebase";
 import { useDispatch, useSelector } from "react-redux";
 import { onAuthStateChanged } from "firebase/auth";
@@ -18,7 +18,6 @@ import { updateCart, updateTotal } from "./store/cart";
 function App() {
 	const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
 	const userUid = useSelector((state) => state.user.userUid);
-	const cartTotal = useSelector((state) => state.cart.cartTotal);
 	const dispatch = useDispatch();
 
 	useEffect(() => {
@@ -59,14 +58,36 @@ function App() {
 		}
 	}, [isAuthenticated]);
 
+	function ProtectedRoute({ children }) {
+		if (!isAuthenticated) {
+			return <Navigate to="/login" />;
+		}
+
+		return children;
+	}
+
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/products/:productId" element={<ProductDetails />} />
 			<Route path="/login" element={<Login />} />
 			<Route path="/signup" element={<Signup />} />
-			<Route path="/cart" element={<ShoopingCart />} />
-			<Route path="/wishlist" element={<Wishlist />} />
+			<Route
+				path="/cart"
+				element={
+					<ProtectedRoute>
+						<ShoopingCart />
+					</ProtectedRoute>
+				}
+			/>
+			<Route
+				path="/wishlist"
+				element={
+					<ProtectedRoute>
+						<Wishlist />
+					</ProtectedRoute>
+				}
+			/>
 			<Route path="*" element={<PageNotFound />} />
 		</Routes>
 	);
